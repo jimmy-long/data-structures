544553b10f5ae55c0cb59f49db185fa599c48040
jimmy-long <jimmy.long.nc@gmail.com>
2020-04-10 12:18:44 -0400

    Created skeleton of LinkedList.

---
+++ DataStructures/src/com/jwlong/datastructures/LinkedList.java
@@ -0,0 +1,79 @@
+package com.jwlong.datastructures;
+
+import java.util.Iterator;
+
+public class LinkedList<E> implements List<E> {
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(E value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(E value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add(E value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(int index, E value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAll(List<E> other) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(int index, E value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
